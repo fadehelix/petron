eef37e60ac9524e0cdce3163dd0b774821579dc9
Adam D <fadehelix@gmail.com>
2020-08-05 14:00:40 +0200

    Add endpoint with data for incomeInAllStationForLastWeek chart

--- db/index.js
+++ db/index.js
@@ -1,6 +1,15 @@
 const faker = require('faker');
 
 const cities = ['New York', 'Buffalo', 'Rochester', 'Albany'];
+const dateRangeWeek = [
+  '01.07',
+  '02.07',
+  '03.07',
+  '04.07',
+  '05.07',
+  '06.07',
+  '07.07',
+];
 
 const generateStations = (howMany = 1) => {
   const stations = [];
@@ -50,11 +59,31 @@ const getNetIncomePerDay = (daysRange) => {
 module.exports = () => {
   const stationsEndpoint = generateStations(5);
 
+  const incomeInAllStations = (stations) => {
+    let obj = {};
+
+    for (let i = 0; i < stations.length; i++) {
+      obj[stations[i].code] = Math.round(Math.random() * 1000);
+    }
+
+    return obj;
+  };
+
+  console.log(incomeInAllStations);
+
   return {
     stations: stationsEndpoint.map((station) => ({
       ...station,
       dailyNetIncomePerHour: getNetIncomePerHour(6, 24),
       weeklyNetIncomePerDay: getNetIncomePerDay(7),
     })),
+    dailyIncomeByStation: dateRangeWeek.map((day, index) => {
+      return {
+        name: day,
+        amt: index * 100,
+        keys: Object.keys(incomeInAllStations(stationsEndpoint)),
+        ...incomeInAllStations(stationsEndpoint),
+      };
+    }),
   };
 };
